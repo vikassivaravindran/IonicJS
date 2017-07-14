e3f50b1b3cca5dedef6d9243ee97499c1d5dba57
vikas <vikas.sivaravindran@accenture.com>
2017-07-14 10:45:23 +0530

    Pushing Sample Ionic App

--- www/js/app.js
+++ www/js/app.js
@@ -23,14 +23,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,47 @@
-angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  var app = angular.module("mainApp", []);  
+        app.controller('CRUDController', function ($scope) {  
+  
+            $scope.EmpModel = {  
+                Id: 0,  
+                Salary: 0,  
+                Name: '',  
+            };  
+  
+            $scope.EmpList = [];  
+            $scope.AddData = function () {  
+                var _emp = {  
+                    Id: $scope.EmpList.length + 1,  
+                    Name: $scope.EmpModel.Name,  
+                    Salary: $scope.EmpModel.Salary  
+                };  
+                $scope.EmpList.push(_emp);  
+                ClearModel();  
+            }  
+  
+            $scope.DeleteData = function (emp) {  
+                var _index = $scope.EmpList.indexOf(emp);  
+                $scope.EmpList.splice(_index, 1);  
+            }  
+  
+            $scope.BindSelectedData = function (emp) {  
+                $scope.EmpModel.Id = emp.Id;  
+                $scope.EmpModel.Name = emp.Name;  
+                $scope.EmpModel.Salary = emp.Salary;  
+            }  
+  
+            $scope.UpdateData = function () {  
+                $.grep($scope.EmpList, function (e) {  
+                    if (e.Id == $scope.EmpModel.Id) {  
+                        e.Name = $scope.EmpModel.Name;  
+                        e.Salary = $scope.EmpModel.Salary;  
+                    }  
+                });  
+                ClearModel();  
+            }  
+  
+            function ClearModel() {  
+                $scope.EmpModel.Id = 0;  
+                $scope.EmpModel.Name = '';  
+                $scope.EmpModel.Salary = 0;  
+            }  
+        });  
